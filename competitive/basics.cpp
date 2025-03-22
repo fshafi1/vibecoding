9572c49d1536de682f320b14ca604dbd20608c20
fshafi1 <fshafi1@umbc.edu>
2025-03-22 09:46:20 -0400

    added some cpp coding

---
+++ competitive/basics.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// define sum function
+void printSum(int a, int b)
+{
+    cout << a + b << endl;
+}
+
+int main()
+{
+    // take input
+    int num1, num2;
+    cin >> num1 >> num2;
+    printSum(num1, num2);
+    return 0;
+}
